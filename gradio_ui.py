bd3738c9b08b020c779c605e570971af48c7f26a
joejoe2 <7013601@gmail.com>
2025-03-25 19:53:42 +0800

    refactor and add image input

--- gradio_ui.py
+++ gradio_ui.py
@@ -1,15 +1,23 @@
 import json
 import threading
+from typing import Optional
 
 import gradio as gr
+from PIL.Image import Image
+from requests import session
 from smolagents import (
-    stream_to_gradio,
+    # stream_to_gradio,
     populate_template,
     OpenAIServerModel,
     CodeAgent,
     ActionStep,
     TaskStep,
+    handle_agent_output_types,
+    AgentText,
+    AgentImage,
+    AgentAudio,
 )
+from smolagents.gradio_ui import pull_messages_from_step
 
 from utils import load_tools
 
@@ -29,6 +37,71 @@ update_tool_prompt_template = (
 )
 
 
+def clean_memory_to_save_tokens(agent: CodeAgent):
+    # modify memory
+    for previous_memory_step in agent.memory.steps:
+        if isinstance(previous_memory_step, TaskStep):
+            # remove update_tool_prompt to save tokens
+            pos = previous_memory_step.task.rfind(update_tool_prompt_prefix)
+            if pos != -1:
+                previous_memory_step.task = previous_memory_step.task[:pos].strip()
+
+
+def stream_to_gradio(
+    agent: CodeAgent,
+    task: str,
+    reset_agent_memory: bool = False,
+    images: list[Image] = None,
+    additional_args: Optional[dict] = None,
+):
+    """Runs an agent with the given task and streams the messages from the agent as gradio ChatMessages."""
+    total_input_tokens = 0
+    total_output_tokens = 0
+
+    for step_log in agent.run(
+        task,
+        stream=True,
+        reset=reset_agent_memory,
+        images=images,
+        additional_args=additional_args,
+    ):
+        # Track tokens if model provides them
+        if getattr(agent.model, "last_input_token_count", None) is not None:
+            total_input_tokens += agent.model.last_input_token_count
+            total_output_tokens += agent.model.last_output_token_count
+            if isinstance(step_log, ActionStep):
+                step_log.input_token_count = agent.model.last_input_token_count
+                step_log.output_token_count = agent.model.last_output_token_count
+
+        for message in pull_messages_from_step(
+            step_log,
+        ):
+            yield message
+
+    final_answer = step_log  # Last log is the run's final_answer
+    final_answer = handle_agent_output_types(final_answer)
+
+    if isinstance(final_answer, AgentText):
+        yield gr.ChatMessage(
+            role="assistant",
+            content=f"**Final answer:**\n{final_answer.to_string()}\n",
+        )
+    elif isinstance(final_answer, AgentImage):
+        yield gr.ChatMessage(
+            role="assistant",
+            content={"path": final_answer.to_string(), "mime_type": "image/png"},
+        )
+    elif isinstance(final_answer, AgentAudio):
+        yield gr.ChatMessage(
+            role="assistant",
+            content={"path": final_answer.to_string(), "mime_type": "audio/wav"},
+        )
+    else:
+        yield gr.ChatMessage(
+            role="assistant", content=f"**Final answer:** {str(final_answer)}"
+        )
+
+
 class MyGradioUI:
     def __init__(
         self, api_base: str, api_key: str, model: str, authorized_imports: list[str]
@@ -43,22 +116,11 @@ class MyGradioUI:
             model_id=self.model, api_base=self.api_base, api_key=self.api_key
         )
 
-        def update_memory(memory_step: ActionStep, agent: CodeAgent):
-            for previous_memory_step in agent.memory.steps:
-                if isinstance(previous_memory_step, TaskStep):
-                    # remove update_tool_prompt to save tokens
-                    pos = previous_memory_step.task.rfind(update_tool_prompt_prefix)
-                    if pos != -1:
-                        previous_memory_step.task = previous_memory_step.task[
-                            :pos
-                        ].strip()
-
         session["agent"] = CodeAgent(
             tools=[],
             model=model,
-            verbosity_level=2,
             additional_authorized_imports=self.authorized_imports,
-            step_callbacks=[update_memory],
+            verbosity_level=2,
         )
         session["available_tools"] = load_tools()
         session["lock"] = threading.Lock()
@@ -80,7 +142,7 @@ class MyGradioUI:
                     try:
                         session["agent"].tools[tool_cls.name] = tool_cls()
                     except Exception as e:
-                        print("Cannot create {} with {}".format(tool_cls, e))
+                        raise Exception("Cannot create {} with {}".format(tool_cls, e))
             print("udpate tools: {}".format(session["agent"].tools))
             tools = list(session["agent"].tools.keys())
             tools.remove("final_answer")
@@ -106,7 +168,7 @@ class MyGradioUI:
                     try:
                         session["agent"].tools[tool_cls.name] = tool_cls()
                     except Exception as e:
-                        print("Cannot create {} with {}".format(tool_cls, e))
+                        raise Exception("Cannot create {} with {}".format(tool_cls, e))
             print("reload tools: {}".format(session["agent"].tools))
             # rerun updated options
             tools = list(session["agent"].tools.keys())
@@ -147,19 +209,10 @@ class MyGradioUI:
                             ),
                         )
                     except Exception as e:
-                        print(
+                        raise Exception(
                             "Cannot create {} with {}, {}".format(
                                 tool_cls, kwargs_str, e
                             )
-                        )
-                        return (
-                            gr.Button("Update Kwargs"),
-                            gr.Dropdown(label="Tool Kwargs"),
-                            gr.Code(
-                                value=json.dumps(tool.kwargs, indent=4),
-                                language="json",
-                                interactive=True,
-                            ),
                         )
 
         return (
@@ -186,9 +239,23 @@ class MyGradioUI:
                     )
         return gr.Code(value="{}", language="json")
 
+    def upload_image(self, image: Image, session):
+        with session["lock"]:
+            # pil image or None
+            if image is None:
+                session["image_input"] = None
+            else:
+                session["image_input"] = image
+
     def interact_with_agent(self, prompt, messages, session_state):
         try:
+            if not isinstance(prompt, str) or len(prompt.strip()) == 0:
+                raise ValueError("Cannot interact with empty prompt !")
             with session_state["lock"]:
+                # save tokens
+                clean_memory_to_save_tokens(session_state["agent"])
+
+                # display messages to user
                 messages.append(gr.ChatMessage(role="user", content=prompt))
                 yield messages
 
@@ -198,15 +265,29 @@ class MyGradioUI:
                     variables={"tools": session_state["agent"].tools},
                 )
                 prompt = prompt + "\n\n" + update_tool_prompt
+
+                # get image input
+                images: list[Image] = (
+                    [session_state["image_input"].copy()]
+                    if (
+                        "image_input" in session_state
+                        and isinstance(session_state["image_input"], Image)
+                    )
+                    else None
+                )
+
                 for msg in stream_to_gradio(
-                    session_state["agent"], task=prompt, reset_agent_memory=False
+                    session_state["agent"],
+                    task=prompt,
+                    reset_agent_memory=False,
+                    images=images,
                 ):
                     messages.append(msg)
                     yield messages
 
                 yield messages
         except Exception as e:
-            print(f"Error in interaction: {str(e)}")
+            print(f"Error in interaction: {str(e.with_traceback())}")
             messages.append(
                 gr.ChatMessage(role="assistant", content=f"Error: {str(e)}")
             )
@@ -225,29 +306,63 @@ class MyGradioUI:
         )
 
     def launch(self, **kwargs):
-        with gr.Blocks(theme="ocean", fill_height=True) as demo:
+        with gr.Blocks(
+            theme="ocean", fill_height=True, css="footer{display:none !important}"
+        ) as demo:
             # Add session state to store session-specific data
             session_state = gr.State({})
             stored_messages = gr.State([])
             file_uploads_log = gr.State([])
 
-            # Main chat interface
-            chatbot = gr.Chatbot(
-                label="Agent",
-                type="messages",
-                avatar_images=(
-                    None,
-                    "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/smolagents/mascot_smol.png",
-                ),
-                resizeable=True,
-            )
-            text_input = gr.Textbox(
-                lines=2,
-                label="Chat Message",
-                container=False,
-                placeholder="Enter your prompt here.",
-            )
-            submit_btn = gr.Button("Submit", variant="primary", interactive=False)
+            with gr.Tab("Agent"):
+                # main chat interface
+                chatbot = gr.Chatbot(
+                    label="Agent",
+                    type="messages",
+                    avatar_images=(
+                        None,
+                        "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/smolagents/mascot_smol.png",
+                    ),
+                    resizeable=True,
+                )
+
+                with gr.Row():
+                    # chat input
+                    text_input = gr.Textbox(
+                        lines=2,
+                        scale=10,
+                        label="Chat Message",
+                        container=False,
+                        placeholder="Enter your prompt here.",
+                    )
+                    with gr.Column(scale=1, min_width=20):
+                        # submit btn
+                        submit_btn = gr.Button(
+                            ">", variant="primary", interactive=False
+                        )
+                        # image input
+                        image_input = gr.Image(
+                            format="png",
+                            type="pil",
+                            sources=["upload"],
+                            label="",
+                            placeholder="",
+                            height=30,
+                        )
+
+            with gr.Tab("Setting"):
+                # dynamically tool selection
+                tool_selections = gr.Dropdown(
+                    choices=[], label="Tools", multiselect=True
+                )
+
+                # customize tool kwargs
+                tool_kwargs_selections = gr.Dropdown(choices=[], label="Tool Kwargs")
+                tool_kwargs_input = gr.Code(value="{}", language="json")
+                tool_kwargs_submit = gr.Button("Update Kwargs")
+
+                # reload tools
+                reload_tool_btn = gr.Button("Reload Tools")
 
             # Set up event handlers
             text_input.submit(
@@ -266,6 +381,7 @@ class MyGradioUI:
                 None,
                 [text_input, submit_btn],
             )
+            image_input.change(self.upload_image, inputs=[image_input, session_state])
 
             submit_btn.click(
                 self.log_user_message,
@@ -283,17 +399,6 @@ class MyGradioUI:
                 None,
                 [text_input, submit_btn],
             )
-
-            # dynamically tool selection
-            tool_selections = gr.Dropdown(choices=[], label="Tools", multiselect=True)
-
-            # customize tool kwargs
-            tool_kwargs_selections = gr.Dropdown(choices=[], label="Tool Kwargs")
-            tool_kwargs_input = gr.Code(value="{}", language="json")
-            tool_kwargs_submit = gr.Button("Update Kwargs")
-
-            # reload tools
-            reload_tool_btn = gr.Button("Reload Tools")
 
             # events
             tool_selections.change(
